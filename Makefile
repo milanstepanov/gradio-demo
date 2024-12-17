f963c4b68208db3e7ca8475892c93032be3da6cb
Milan Stepanov <milanstepanov89@gmail.com>
2024-12-17 12:37:22 +0100

    Add linting and formating.

--- Makefile
+++ Makefile
@@ -1,3 +1,17 @@
 install:
 	pip install --upgrade pip &&\
-	pip install -r requirements.txt 
+	pip install -r requirements.txt
+
+lint:
+	pylint *.py
+
+
+format:
+	black *.py
+
+
+run:
+	python hello.py
+
+
+all: install,lint, format
--- hello.py
+++ hello.py
@@ -1,8 +1,25 @@
+"""'Docstring for gradio-demo.hello"""
+
 import gradio as gr
 
-def greet(name, intensity):
-    return f"Hello, {name}" + "!"*int(intensity)
 
-demo = gr.Interface(fn=greet, inputs=["text", "slider"],outputs=["text"],)
+def greet(name: str, intensity: int):
+    """'Generate greeting.
+
+    :param name: Name to greet.
+    :type str:
+    :param intensity:
+    :type int:
+    :return: Hello greeting.
+    :rtype: str"""
+
+    return f"Hello, {name}" + "!" * int(intensity)
+
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "slider"],
+    outputs=["text"],
+)
 
 demo.launch()
